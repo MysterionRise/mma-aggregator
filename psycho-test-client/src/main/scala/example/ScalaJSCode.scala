177e4b2479fb8a1f3b59b94dc1373209f9a1c2e9
Konstantin Perikov <kperikov@griddynamics.com>
2015-04-16 13:47:26 +0200

    Add response feedback for first 2 sessions

--- psycho-test-client/src/main/scala/example/ScalaJSCode.scala
+++ psycho-test-client/src/main/scala/example/ScalaJSCode.scala
@@ -11,26 +11,41 @@ import shared.{Image => TestImage}
 object ScalaJSCode extends js.JSApp {
 
   def main(): Unit = {
-    val id: Element = dom.document.getElementById("scalajsShoutOut")
+    val success: Element = dom.document.getElementById("success")
+    val error: Element = dom.document.getElementById("error")
     for (i <- 1 to 8) {
       val e: Image = dom.document.getElementById(i.toString).asInstanceOf[Image]
       e.onclick = {
         (e1: dom.MouseEvent) =>
           dom.document.cookie = ""
+          success.textContent = ""
+          error.textContent = ""
           val user: Heading = dom.document.getElementById("user").asInstanceOf[Heading]
           val userID: String = user.getAttribute("data-user-id")
           val div: Div = dom.document.getElementById("kagan-test").asInstanceOf[Div]
           val pattern: Image = dom.document.getElementById("pattern").asInstanceOf[Image]
           val img: TestImage = constructImage(pattern.src)
           if (img.roundNumber == 1) {
-            dom.document.cookie += s"""PLAY_SESSION=${userID}|${img.imageNumber}|${img.roundNumber}|${System.currentTimeMillis()}"""
+            dom.document.cookie += s"""PLAY_SESSION=${userID}|${e.id}|${img.roundNumber}|${System.currentTimeMillis()}\n"""
           } else {
-            dom.document.cookie += s"""${userID}|${img.imageNumber}|${img.roundNumber}|${System.currentTimeMillis()}"""
+            dom.document.cookie += s"""${userID}|${e.id}|${img.roundNumber}|${System.currentTimeMillis()}\n"""
           }
           img.roundNumber += 1 // move to next round
           img.roundNumber match {
-            case 2 | 3 => {
-              // todo provide correct answer
+            case 2 => {
+              if (e.id.equals("1")) {
+                success.textContent = "You successfully answer the question!"
+              } else {
+                error.textContent = " Your answer is INCORRECT!"
+              }
+              constructNewRound(pattern, img)
+            }
+            case 3 => {
+              if (e.id.equals("5")) {
+                success.textContent = "You successfully answer the question!"
+              } else {
+                error.textContent = " Your answer is INCORRECT!"
+              }
               constructNewRound(pattern, img)
             }
             case x if x > 14 => {
