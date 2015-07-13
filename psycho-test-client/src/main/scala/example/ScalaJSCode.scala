28198e2b2325f46599bfd1c2903f4ed81d419b64
Konstantin Perikov <kperikov@griddynamics.com>
2015-07-13 19:54:54 +0200

    Fix different tests issue

--- psycho-test-client/src/main/scala/example/ScalaJSCode.scala
+++ psycho-test-client/src/main/scala/example/ScalaJSCode.scala
@@ -1,5 +1,6 @@
 package example
 
+import org.scalajs.dom.html.Div
 import org.scalajs.dom.raw.Element
 import scala.scalajs.js
 import org.scalajs.dom
@@ -7,11 +8,20 @@ import org.scalajs.dom
 object ScalaJSCode extends js.JSApp {
 
   def main(): Unit = {
+    val kagan = getElementById[Div]("kagan-test")
+    if (kagan != null) {
+      KaganTest.doTest()
+    }
+    val gothshild = getElementById[Div]("gothshild-test")
+    if (gothshild != null) {
+      GothshildTest.doTest()
+    }
+    val ultraRapid = getElementById[Div]("ultra-rapid")
+    if (ultraRapid != null) {
+      UltraRapidTest.doTest()
+    }
     // todo, need to manage different tests
-    // todo a big problem right now
-    GothshildTest.doTest()
-    UltraRapidTest.doTest()
-    KaganTest.doTest()
+    // todo a big problem right no
   }
 
   def getElementById[T <: Element](name: String): T = {
